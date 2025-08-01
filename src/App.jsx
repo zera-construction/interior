5677e25eb4041d23a9c27bc44d646a60231afae9
ajmal faris <ajmalfaris.me@gmail.com>
2025-08-01 23:02:43 +0530

    feat: add Google Fonts and Material Icons to enhance styling options

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
+import './App.css';
 
 function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // enable class-based dark mode
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Inter'", "sans-serif"], // you can update this later
+      },
+    },
   },
   plugins: [],
-}
+};
