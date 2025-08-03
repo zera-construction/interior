bcdb93557d17669425342da382c9b0334e4f1fc9
ajmal faris <ajmalfaris.me@gmail.com>
2025-08-03 22:57:04 +0530

    feat: enhance Header component with additional social media icons and improve button styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
 // src/components/Header.jsx
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { TiSocialFacebook, TiSocialLinkedin, TiSocialYoutube } from "react-icons/ti";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialYoutube,
+} from "react-icons/ti";
 import { AiFillInstagram } from "react-icons/ai";
 import { RiTwitterXFill } from "react-icons/ri";
 import { IoLogoWhatsapp } from "react-icons/io";
-import zeraWhiteLogo from '/src/assets/logos/zera_white_logo.png';
+import { BsThreads } from "react-icons/bs";
+import { FaTelegramPlane } from "react-icons/fa";
+import { IoCall } from "react-icons/io5";
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
+import zeraWhiteLogo from "/src/assets/logos/zera_white_logo.png";
+
 
 const Header = () => {
   const [darkMode, setDarkMode] = useState(true);
@@ -19,11 +29,10 @@ const Header = () => {
   ];
 
   const socialIcons = [
-
     {
       name: "WhatsApp",
       icon: <IoLogoWhatsapp />,
-      href: "https://wa.me/your-number"
+      href: "https://wa.me/your-number",
     },
     {
       name: "Facebook",
@@ -50,9 +59,22 @@ const Header = () => {
       icon: <RiTwitterXFill />,
       href: "https://x.com",
     },
+    {
+      name: "Threads",
+      icon: <BsThreads />,
+      href: "https://threads.com",
+    },
+    {
+      name: "Call",
+      icon: <IoCall />,
+      href: "https://threads.com",
+    },
+    {
+      name: "Telegram",
+      icon: <FaTelegramPlane />,
+      href: "https://telegram.com",
+    },
   ];
-
-
 
   useEffect(() => {
     const root = window.document.documentElement;
@@ -62,7 +84,6 @@ const Header = () => {
 
   return (
     <header className="flex justify-between items-center px-8 py-4 bg-white dark:bg-black text-black dark:text-white transition-all duration-300">
-
       {/* Logo */}
       <div>
         {darkMode ? (
@@ -88,11 +109,11 @@ const Header = () => {
       {/* Right Side: Theme toggle + menu icon */}
       <div className="flex items-center gap-4">
         {/* Menu Icon */}
-        <button className="text-2xl"
-          onClick={() => setIsOpen(!isOpen)}>
+        <button className="text-2xl" onClick={() => setIsOpen(!isOpen)}>
           <span
-            className={`material-symbols-outlined transition-transform duration-300 ${isOpen ? "rotate-90" : "rotate-0"
-              }`}
+            className={`material-symbols-outlined transition-transform duration-300 ${
+              isOpen ? "rotate-90" : "rotate-0"
+            }`}
           >
             {isOpen ? "close" : "drag_handle"}
           </span>
@@ -103,21 +124,20 @@ const Header = () => {
       {isOpen && (
         <div className="absolute top-[60px] left-0 w-full h-full bg-white dark:bg-black text-black dark:text-white px-6 py-4 flex flex-col gap-4 md:hidden z-10">
           <div className="flex justify-center items-center mb-4 gap-4 mt-6">
-            <button onClick={() => setDarkMode(!darkMode)} className="flex justify-center items-center bg-white dark:bg-black border w-16 h-16 border-2 rounded-2xl shadow-lg">
+            <button
+              onClick={() => setDarkMode(!darkMode)}
+              className="flex justify-center items-center bg-white dark:bg-black border w-16 h-16 border-2 rounded-2xl shadow-lg"
+            >
               <span className="material-symbols-outlined">
                 {darkMode ? "light_mode" : "dark_mode"}
               </span>
             </button>
 
             <button className="flex justify-center items-center bg-white dark:bg-black border w-16 h-16 border-2 rounded-2xl shadow-lg">
-              <span class="material-symbols-outlined">
-                language
-              </span>
+              <span class="material-symbols-outlined">language</span>
             </button>
             <button className="flex justify-center items-center bg-white dark:bg-black border w-16 h-16 border-2 rounded-2xl shadow-lg">
-              <span class="material-symbols-outlined">
-                location_on
-              </span>
+              <span class="material-symbols-outlined">location_on</span>
             </button>
           </div>
           {navItems.map((item) => (
@@ -133,7 +153,7 @@ const Header = () => {
 
           {/* social media icons */}
           <div className="grid grid-cols-3 gap-6 text-xl text-black dark:text-white mt-8 justify-items-center">
-            {socialIcons.map(({ name, icon, href}) => (
+            {socialIcons.map(({ name, icon, href }) => (
               <a
                 key={name}
                 href={href}
@@ -154,32 +174,31 @@ const Header = () => {
              top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
              w-auto h-auto 
              bg-white/20 dark:bg-black/20
-             backdrop-blur-md 
+             backdrop-blur-xl 
              border border-white/50 dark:border-white/20
              rounded-3xl shadow-2xl dark:shadow-white/30
              transition-all duration-500 p-8 z-50"
           onClick={() => setIsOpen(false)}
         >
-          <div className="grid grid-cols-3 gap-10 text-xl text-black dark:text-white justify-items-center">
-            <button onClick={() => setDarkMode(!darkMode)} className="flex justify-center items-center border-2 border-black dark:border-white w-16 h-16 rounded-2xl shadow-xl dark:shadow-gray-500/20 hover:scale-110 transition">
-              <span className="material-symbols-outlined">
-                {darkMode ? "light_mode" : "dark_mode"}
+          <div className="grid grid-cols-4 gap-12 text-xl text-black dark:text-white justify-items-center">
+            <button
+              onClick={() => setDarkMode(!darkMode)}
+              className="flex justify-center items-center border-2 border-gray-400 dark:border-white w-16 h-16 rounded-3xl shadow-xl dark:shadow-gray-500/20 hover:scale-110 transition-all duration-500 bg-white dark:bg-black hover:rotate-[360deg]"
+            >
+              <span>
+                {darkMode ? <MdDarkMode /> : <MdLightMode />}
               </span>
             </button>
 
-            <button className="flex justify-center items-center border-2 border-black dark:border-white w-16 h-16 rounded-2xl shadow-xl dark:shadow-gray-500/20 hover:scale-110 transition">
-              <span class="material-symbols-outlined">
-                language
-              </span>
+            <button className="flex justify-center items-center border-2 border-gray-400 dark:border-white w-16 h-16 rounded-3xl shadow-xl dark:shadow-gray-500/20 hover:scale-110 transition-all duration-500 bg-white dark:bg-black hover:rotate-[360deg]">
+              <span class="material-symbols-outlined">language</span>
             </button>
-            <button className="flex justify-center items-center border-2 border-black dark:border-white w-16 h-16 rounded-2xl shadow-xl dark:shadow-gray-500/20 hover:scale-110 transition">
-              <span class="material-symbols-outlined">
-                location_on
-              </span>
+            <button className="flex justify-center items-center border-2 border-gray-400 dark:border-white w-16 h-16 rounded-3xl shadow-xl dark:shadow-gray-500/20 hover:scale-110 transition-all duration-500 bg-white dark:bg-black hover:rotate-[360deg]">
+              <span class="material-symbols-outlined">location_on</span>
             </button>
 
-            {socialIcons.map(({ name, icon, href, color }) => (
-              <button className="flex justify-center items-center border-2 border-black dark:border-white w-16 h-16 rounded-2xl shadow-xl dark:shadow-gray-500/20 hover:scale-110 transition">
+            {socialIcons.map(({ name, icon, href }) => (
+              <button className="flex justify-center items-center border-2 border-gray-400 dark:border-white w-16 h-16 rounded-3xl shadow-xl dark:shadow-gray-500/20 hover:scale-110 transition-all duration-500 bg-white dark:bg-black hover:rotate-[360deg]">
                 <a
                   key={name}
                   href={href}
