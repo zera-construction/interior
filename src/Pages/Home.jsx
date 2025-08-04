117c2506973b3595d4dbb95eb0bf5d7f14f72694
ajmal faris <ajmalfaris.me@gmail.com>
2025-08-04 23:57:37 +0530

    setUp Home Page components structure for the web layout

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
 import HerorSection from '../components/HerorSection.jsx'
+import ProjectStats from '../components/ProjectStats.jsx'
+import VideoSection from '../components/VideoSection.jsx'
+import ServicesList from '../components/ServicesList.jsx'
+import GallarySlider from '../components/GallerySlider.jsx'
+import StartProject from '../components/StartProject.jsx'
 
 function Home() {
   return (
-    <div className='min-h-screen bg:white dark:bg-black text-gray-800 dark:text-gray-200 px-6'>
+    <div className='min-h-screen bg:white dark:bg-black text-gray-800 dark:text-gray-200 px-6 w-[100vw] overflow-hidden'>
       <Header />
       <HerorSection />
+      <ProjectStats/>
+      <VideoSection/>
+      <ServicesList />
+      <GallarySlider/>
+      <StartProject/>
+      <Header />
     </div>
   )
 }
