2b47ca6358b2cd7572e9003b0ff79bace8fa1e91
ajmal faris <ajmalfaris.me@gmail.com>
2025-08-05 22:06:59 +0530

    refactor: Clean up Home component structure and formatting

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,17 +6,21 @@ import VideoSection from '../components/VideoSection.jsx'
 import ServicesList from '../components/ServicesList.jsx'
 import GallarySlider from '../components/GallerySlider.jsx'
 import StartProject from '../components/StartProject.jsx'
+import Collections from "../components/collections.jsx"
+import Footer from '../components/Footer.jsx'
 
 function Home() {
   return (
     <div className='min-h-screen bg:white dark:bg-black text-gray-800 dark:text-gray-200 px-6 w-[100vw] overflow-hidden'>
       <Header />
       <HerorSection />
-      <ProjectStats/>
-      <VideoSection/>
+      <ProjectStats />
+      <VideoSection />
       <ServicesList />
-      <GallarySlider/>
-      <StartProject/>
+      <GallarySlider />
+      <Collections />
+      <StartProject />
+      <Footer />
       <Header />
     </div>
   )
