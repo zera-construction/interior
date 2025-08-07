a7c7e1b6573a1039119465b10e30402069bfe19d
ajmal faris <ajmalfaris.me@gmail.com>
2025-08-07 08:27:12 +0530

    removed unwanted styles from home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
-import HerorSection from '../components/HerorSection.jsx'
+import HeroSection from '../components/HeroSection.jsx'
 import ProjectStats from '../components/ProjectStats.jsx'
 import VideoSection from '../components/VideoSection.jsx'
 import ServicesList from '../components/ServicesList.jsx'
@@ -9,11 +9,12 @@ import StartProject from '../components/StartProject.jsx'
 import Collections from "../components/collections.jsx"
 import Footer from '../components/Footer.jsx'
 
+
 function Home() {
   return (
-    <div className='min-h-screen bg:white dark:bg-black text-gray-800 dark:text-gray-200 px-6 w-[100vw] overflow-hidden'>
+    <div className='min-h-screen bg-background px-6'>
       <Header />
-      <HerorSection />
+      <HeroSection />
       <ProjectStats />
       <VideoSection />
       <ServicesList />
